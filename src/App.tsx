62e0036181fdc00182097fa48279789aea768207
Umair Jameel <umairjameel321@gmail.com>
2024-02-07 20:30:50 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,80 @@
-import { 
-    Refine,
-    GitHubBanner, 
-    WelcomePage,
-    Authenticated
-,AuthPage,ErrorComponent, 
-} from '@refinedev/core';
+import {
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+  AuthPage,
+  ErrorComponent,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
+import { HeadlessInferencer } from "@refinedev/inferencer/headless";
 
 import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
-import routerBindings, { NavigateToResource, CatchAllNavigate, UnsavedChangesNotifier, DocumentTitleHandler } from "@refinedev/react-router-v6";
+import routerBindings, {
+  NavigateToResource,
+  CatchAllNavigate,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
 import { Layout } from "./components/layout";
 import "./App.css";
 
-
-
-
-
 function App() {
-    
-
-    
-    
-    return (
-        <BrowserRouter>
-        <GitHubBanner />
-        <RefineKbarProvider>
-            
-            <DevtoolsProvider>
-                <Refine routerProvider={routerBindings}
-dataProvider={dataProvider("https://api.fake-rest.refine.dev")} 
-                    options={{
-                        syncWithLocation: true,
-                        warnWhenUnsavedChanges: true,
-                        useNewQueryKeys: true,
-                            projectId: "IE5K2Q-AqgY4W-i7vSbP",
-                        
-                    }}
-                >
-
-
-                        <Routes>
-                            <Route index element={<WelcomePage />} />
-                        </Routes>
-                    <RefineKbar />
-                    <UnsavedChangesNotifier />
-                    <DocumentTitleHandler />
-                </Refine>
-            <DevtoolsPanel />
-            </DevtoolsProvider>
-            
-        </RefineKbarProvider>
-        </BrowserRouter>
-      );
-};
+  return (
+    <BrowserRouter>
+      {/* <GitHubBanner /> */}
+      <RefineKbarProvider>
+        <DevtoolsProvider>
+          <Refine
+            routerProvider={routerBindings}
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            resources={[
+              {
+                name: "blog_posts",
+                list: "/blog-posts",
+                show: "/blog-posts/show/:id",
+                create: "/blog-posts/create",
+                edit: "/blog-posts/edit/:id",
+              },
+            ]}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              useNewQueryKeys: true,
+              projectId: "IE5K2Q-AqgY4W-i7vSbP",
+            }}
+          >
+            <Routes>
+              <Route
+                element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }
+              >
+                <Route
+                  index
+                  element={<NavigateToResource resource="blog_posts" />}
+                />
+                <Route path="blog-posts">
+                  <Route index element={<HeadlessInferencer />} />
+                  <Route path="show/:id" element={<HeadlessInferencer />} />
+                  <Route path="edit/:id" element={<HeadlessInferencer />} />
+                  <Route path="create" element={<HeadlessInferencer />} />
+                </Route>
+              </Route>
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
 
 export default App;
